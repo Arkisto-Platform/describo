46a77f187a0e6c1b687c3ebfad359e8d35521381
Marco La Rosa <m@lr.id.au>
2020-03-17 16:18:00 +1100

    remove text component from mappings

--- src/components/CrateCreator/CoreEntities/component.mixins.js
+++ src/components/CrateCreator/CoreEntities/component.mixins.js
@@ -1,6 +1,5 @@
 export default {
     components: {
-        TextComponent: () => import("./Text.component.vue"),
         PersonComponent: () => import("./Person.component.vue"),
         OrganisationComponent: () => import("./Organisation.component.vue")
     }
