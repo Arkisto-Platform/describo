92aefeb64f39fba8363ea59cc9d317ffc3492510
Marco La Rosa <m@lr.id.au>
2020-03-19 15:47:57 +1100

    rework introduction

--- src/components/IntroductionShell.component.vue
+++ src/components/IntroductionShell.component.vue
@@ -1,34 +1,36 @@
 <template>
-    <div class="flex flex-col justify-around style-introduction text-xl">
-        <p class="text-3xl">Welcome to describo!</p>
-        <p>
-            Describo is a tool to create and update Research Object Crates
-            (RO-Crate).
-        </p>
-        <p>
-            You have some data and you wish to describe it in a standard way
-            such that others can discover and use it. Describo is the tool for
-            the job!
-        </p>
-        <p>
-            Get started by creating a new Research Object crate or loading an
-            existing one.
-        </p>
-        <div class="flex flex-row justify-around mt-6">
-            <div class="flex flex-col w-64 text-center">
-                <div
-                    @click="$router.push({ path: '/describe' })"
-                    class="cursor-pointer bg-gray-200 px-8 py-12 rounded-lg text-3xl hover:bg-orange-400"
-                >
-                    Get Started!
-                </div>
-            </div>
+    <div class="flex flex-col">
+        <div class="flex flex-col style-introduction text-xl" v-if="!target && !profile">
+            <p class="text-3xl">Welcome to describo!</p>
+            <p class="my-2">
+                Describo is a tool to create and update Research Object Crates
+                (RO-Crate).
+            </p>
+            <p class="my-2">
+                You have some data and you wish to describe it in a standard way
+                such that others can discover and use it. Describo is the tool for
+                the job!
+            </p>
         </div>
+        <describe-component />
     </div>
 </template>
 
 <script>
+import DescribeComponent from "components/Describe.component.vue";
+
 export default {
+    components: {
+        DescribeComponent
+    },
+    computed: {
+        target: function() {
+            return this.$store.state.target;
+        },
+        profile: function() {
+            return this.$store.state.profile;
+        }
+    },
     data() {
         return {};
     }
@@ -36,7 +38,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.style-introduction {
-    height: 400px;
-}
 </style>
--- src/renderer/routes.js
+++ src/renderer/routes.js
@@ -6,7 +6,6 @@ Vue.use(VueRouter);
 
 import ShellComponent from "components/Shell.component.vue";
 import BadRequestComponent from "components/BadRequest.component.vue";
-import DescribeShellComponent from "components/DescribeShell.component.vue";
 import IntroductionShellComponent from "components/IntroductionShell.component.vue";
 
 export const router = new VueRouter({
@@ -19,10 +18,6 @@ export const router = new VueRouter({
                 {
                     path: "introduction",
                     component: IntroductionShellComponent
-                },
-                {
-                    path: "describe",
-                    component: DescribeShellComponent
                 }
             ]
         }
