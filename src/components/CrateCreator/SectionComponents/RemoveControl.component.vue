2db4921387a2bf73f0e13ff0cd66f798b150daad
Marco La Rosa <m@lr.id.au>
2020-03-20 11:06:59 +1100

    implement control to remove simple properties

---
+++ src/components/CrateCreator/SectionComponents/RemoveControl.component.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="mx-2">
+        <el-button
+            @click="cancel(type)"
+            type="danger"
+            v-for="(type, idx) of types"
+            :key="idx"
+        >
+            <i class="fas fa-trash-alt"></i>
+        </el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String | Array,
+            required: true
+        }
+    },
+    data() {
+        return {
+            types: []
+        };
+    },
+    mounted() {
+        this.types = this.type;
+        if (typeof this.type === "string") {
+            this.types = [this.type];
+        }
+    },
+    methods: {
+        cancel() {
+            this.$emit("cancel");
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
