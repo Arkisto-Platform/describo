12c87ad7abf66c8818eeb92d8b9e5cb92a412fd8
Marco La Rosa <m@lr.id.au>
2020-04-28 09:44:15 +1000

    revise styling and layout

--- src/components/CrateCreator/shared/RenderProfileItem.component.vue
+++ src/components/CrateCreator/shared/RenderProfileItem.component.vue
@@ -2,18 +2,18 @@
     <div class="flex flex-row">
         <div class="cursor-pointer bg-green-200 p-2 rounded-l-lg" @click="edit">
             <div class="flex flex-col space-y-1">
+                <div class="text-gray-600 text-xs">
+                    @id&nbsp;
+                    {{ itemData.uuid }}
+                </div>
                 <div class="text-gray-800 text-lg">
                     {{ itemData["@type"] }}:
                     {{ itemData.name }}
                 </div>
-                <div class="text-gray-600 text-xs pt-2">
-                    @id&nbsp;
-                    {{ itemData.uuid }}
-                </div>
             </div>
         </div>
         <div class="bg-green-200 p-2 rounded-r-lg">
-            <el-button @click="removeItem" size="mini" class="mt-3">
+            <el-button @click="removeItem" size="mini" class="rounded-lg">
                 <i class="fas fa-times"></i>
             </el-button>
         </div>
