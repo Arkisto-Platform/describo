bf15f613559384f46e0aac9503143ed03280c4c6
Marco La Rosa <m@lr.id.au>
2020-05-04 15:33:08 +1000

    add crate write to disk functionality
    This dries up the earlier implementation which was in multiple places.
    Also, it ensures the save state is always visible.

--- src/components/CrateCreator/ShellControls.component.vue
+++ src/components/CrateCreator/ShellControls.component.vue
@@ -32,6 +32,30 @@
                     crate loading errors
                 </el-button>
             </div>
+            <div class="border-l pl-2 border-black cursor-pointer">
+                <el-button @click="toggleCrateWrite" size="mini">
+                    <div
+                        v-show="!enableWriteToDisk"
+                        class="pointer text-red-600 text-xl"
+                    >
+                        <i class="far fa-save"></i> save to disk disabled
+                    </div>
+                    <div
+                        v-show="enableWriteToDisk"
+                        class="cursor-pointer text-green-600 text-xl"
+                    >
+                        <i class="far fa-save"></i> save to disk enabled
+                    </div>
+                </el-button>
+            </div>
+            <div class="flex flex-row text-xl pt-1">
+                <div v-show="saving" class="text-orange-600">
+                    <i class="fas fa-save"></i> saving the crate
+                </div>
+                <div v-show="saved" class="text-green-600">
+                    <i class="fas fa-check"></i> saved
+                </div>
+            </div>
             <div class="flex-grow"></div>
             <div>
                 <el-button
@@ -78,18 +102,12 @@
 </template>
 
 <script>
-import {
-    cloneDeep,
-    isObject,
-    isPlainObject,
-    isArray,
-    groupBy,
-    round,
-} from "lodash";
 import DataInspectorComponent from "./shared/DataInspector.component.vue";
 import CrateExportComponent from "./CrateExport/CrateExport.component.vue";
 import CrateLoadingErrorsComponent from "./shared/CrateLoadingErrors.component.vue";
 import AddItemToCrateComponent from "./shared/AddItemToCrate.component.vue";
+import CrateTool from "components/CrateCreator/crate-tools";
+const crateTool = new CrateTool();
 
 export default {
     props: {
@@ -112,9 +130,61 @@ export default {
                 crateExportDialog: false,
                 crateLoadingErrors: false,
             },
+            saved: false,
+            saving: false,
         };
     },
-    methods: {},
+    computed: {
+        graph: function() {
+            return this.$store.state.graph;
+        },
+        enableWriteToDisk: function() {
+            return this.$store.state.enableWriteToDisk;
+        },
+    },
+    watch: {
+        "$store.state.graph": function() {
+            this.writeCrateToDisk();
+        },
+    },
+    methods: {
+        toggleCrateWrite() {
+            const current = this.$store.state.enableWriteToDisk;
+            this.saving = false;
+            this.saved = false;
+            this.$store.commit("setWriteToDisk", !current);
+        },
+        async writeCrateToDisk() {
+            if (!this.enableWriteToDisk) return;
+            this.error = undefined;
+            this.saved = false;
+            this.saving = true;
+            try {
+                this.valid = crateTool.verifyCrate({
+                    data: this.$store.state.graph,
+                    inputs: this.$store.state.profileInputs.inputs,
+                });
+                crateTool.assembleCrate({ data: this.$store.state.graph });
+                if (this.enableCrateWriteToDisk) {
+                    await crateTool.writeCrate({
+                        target: this.$store.state.target,
+                    });
+                }
+                await new Promise((resolve) => setTimeout(resolve, 1500));
+                this.saving = false;
+                this.saved = true;
+                await new Promise((resolve) => setTimeout(resolve, 1500));
+                this.saved = false;
+            } catch (error) {
+                this.error = {
+                    title: "There was a problem saving the crate.",
+                    description: error.message,
+                };
+                this.saving = false;
+                this.saved = false;
+            }
+        },
+    },
 };
 </script>
 
