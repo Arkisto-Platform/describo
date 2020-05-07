f9c6b46bf696e15b0e202edc0b7d701330462684
Marco La Rosa <m@lr.id.au>
2020-05-07 11:00:17 +1000

    revise view - closes #45
    - make whole row editable
    - make identifier display consistent with item boxes in render profile
    - move action buttons to the left

--- src/components/CrateCreator/CrateParts/PartsList.component.vue
+++ src/components/CrateCreator/CrateParts/PartsList.component.vue
@@ -12,7 +12,7 @@
             </div>
             <div class="flex-grow"></div>
             <el-pagination
-                v-if="total > pageSize + 1"
+                v-if="total > pageSize"
                 layout="prev, pager, next"
                 :page-size="pageSize"
                 :total="total"
@@ -22,7 +22,32 @@
         </div>
 
         <el-table :data="parts" class="cursor-pointer" @row-click="editPart">
-            <el-table-column prop="uuid" label="Identifier" width="400">
+            <el-table-column prop="" label="Actions" width="80">
+                <template slot-scope="scope">
+                    <el-button
+                        type="success"
+                        @click="editPart(scope.row)"
+                        size="small"
+                    >
+                        <i class="fas fa-edit"></i>
+                    </el-button>
+                </template>
+            </el-table-column>
+            <el-table-column prop="uuid" label="Identifier" width="600">
+                <template slot-scope="scope">
+                    <div class="bg-green-200 px-2 py-1 rounded-lg">
+                        <div class="flex flex-col space-y-1">
+                            <div class="text-gray-600 text-xs">
+                                @id&nbsp;
+                                {{ scope.row.uuid }}
+                            </div>
+                            <div class="text-gray-800 text-base">
+                                {{ scope.row["@type"] }}:
+                                {{ scope.row.name }}
+                            </div>
+                        </div>
+                    </div>
+                </template>
             </el-table-column>
             <el-table-column prop="" label="Type" width="80" align="center">
                 <template slot-scope="scope" class="text-center">
@@ -38,13 +63,6 @@
                     >
                         <i class="fas fa-file fa-2x text-green-400"></i>
                     </div>
-                </template>
-            </el-table-column>
-            <el-table-column prop="" label="Actions">
-                <template slot-scope="scope">
-                    <el-button type="success" @click="editPart(scope.row)">
-                        <i class="fas fa-edit"></i>
-                    </el-button>
                 </template>
             </el-table-column>
         </el-table>
