7d68f427fd05f6f1667dc4a8a6bbcafba98f8ee5
Marco La Rosa <m@lr.id.au>
2020-05-08 15:02:14 +1000

    add capability to delete local profile; revise wording

--- src/components/ProfileSelection/LoadExternalProfile.component.vue
+++ src/components/ProfileSelection/LoadExternalProfile.component.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="flex flex-row">
         <div class="text-lg text-right pr-10 pt-12 w-1/2 border-r-2">
-            Or load a custom profile.
+            Or load a domain specific profile.
         </div>
         <div class="flex flex-col w-1/2 pl-2 space-y-6">
             <div class="flex flex-row">
                 <div class="pt-3 mr-2">
-                    Load a custom profile from a local file.
+                    Load from a local file.
                 </div>
                 <div class="my-2">
                     <el-button
@@ -20,7 +20,7 @@
                 </div>
             </div>
             <div class="flex flex-col my-2">
-                <div>Load a custom profile from a URL.</div>
+                <div>Load from a URL.</div>
                 <div class="flex flex-row space-x-2">
                     <div class="flex-grow">
                         <el-input type="text" v-model="url"></el-input>
@@ -36,15 +36,31 @@
                 </div>
             </div>
             <div class="flex flex-col">
-                <div>Select a previously loaded custom profile.</div>
+                <div>Select a previously loaded profile.</div>
                 <div
                     v-for="profile of profiles"
                     :key="profile.name"
                     class="cursor-pointer hover:text-orange-500"
-                    @click="loadProfile(profile)"
                 >
-                    <i class="fas fa-chevron-right"></i>&nbsp;
-                    {{ profile.name }} (version: {{ profile.version }})
+                    <div class="flex flex-row">
+                        <div>
+                            <el-button
+                                @click="removeProfile(profile)"
+                                type="danger"
+                                class="mr-1"
+                                size="small"
+                            >
+                                <i class="fas fa-trash-alt"></i>
+                            </el-button>
+                        </div>
+                        <div
+                            class="pt-1 flex-grow"
+                            @click="loadProfile(profile)"
+                        >
+                            <i class="fas fa-chevron-right"></i>&nbsp;
+                            {{ profile.name }} (version: {{ profile.version }})
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -60,12 +76,10 @@ import isUrl from "validator/lib/isUrl";
 
 export default {
     data() {
-        return { url: undefined };
-    },
-    computed: {
-        profiles: function() {
-            return JSON.parse(localStorage.getItem("profiles"));
-        },
+        return {
+            url: undefined,
+            profiles: JSON.parse(localStorage.getItem("profiles")),
+        };
     },
     methods: {
         async selectFolder() {
@@ -122,6 +136,12 @@ export default {
         },
         loadProfile(profile) {
             this.$emit("store-profile", profile.name);
+        },
+        removeProfile(profile) {
+            let profiles = JSON.parse(localStorage.getItem("profiles"));
+            profiles = profiles.filter((p) => p.name !== profile.name);
+            this.profiles = profiles;
+            localStorage.setItem("profiles", JSON.stringify(profiles));
         },
     },
 };
