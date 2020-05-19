d1a0ec26d2da3342eae349815c7c28034e236539
Marco La Rosa <m@lr.id.au>
2020-05-19 12:03:54 +1000

    handle api issues gracefully

--- github-pages/src/components/Releases.component.vue
+++ github-pages/src/components/Releases.component.vue
@@ -1,33 +1,36 @@
 <template>
     <div class="flex flex-col">
-        <div class="md:text-3xl text-center">
-            Download Describo for your platform and start describing your data.
-            <div class="text-sm">
-                Describo is an application designed for your desktop so it won't
-                work on your phone or tablet.
+        <div class="flex flex-col" v-if="release">
+            <div class="md:text-3xl text-center">
+                Download Describo for your platform and start describing your
+                data.
+                <div class="text-sm">
+                    Describo is an application designed for your desktop so it
+                    won't work on your phone or tablet.
+                </div>
             </div>
-        </div>
-        <div class="mt-10 flex flex-row justify-around">
-            <div class="hover:text-orange-600">
-                <a :href="releaseUrl('windows')">
-                    <i class="fab fa-windows fa-6x"></i>
-                </a>
+            <div class="mt-10 flex flex-row justify-around">
+                <div class="hover:text-orange-600">
+                    <a :href="releaseUrl('windows')">
+                        <i class="fab fa-windows fa-6x"></i>
+                    </a>
+                </div>
+                <div class="hover:text-orange-600">
+                    <a :href="releaseUrl('mac')">
+                        <i class="fab fa-apple fa-6x"></i>
+                    </a>
+                </div>
+                <div class="hover:text-orange-600">
+                    <a :href="releaseUrl('linux')">
+                        <i class="fab fa-linux fa-6x"></i>
+                    </a>
+                </div>
             </div>
-            <div class="hover:text-orange-600">
-                <a :href="releaseUrl('mac')">
-                    <i class="fab fa-apple fa-6x"></i>
-                </a>
-            </div>
-            <div class="hover:text-orange-600">
-                <a :href="releaseUrl('linux')">
-                    <i class="fab fa-linux fa-6x"></i>
-                </a>
-            </div>
-        </div>
-        <div class="mt-6 text-base">
             <div>Latest release: {{ release.name }}</div>
+        </div>
+        <div class="mt-6 text-base text-center">
             <div>
-                Check out all releases @
+                Check out available releases @
                 <a
                     href="https://github.com/UTS-eResearch/describo/releases"
                     target="_blank"
@@ -49,11 +52,15 @@ export default {
         };
     },
     async mounted() {
-        let response = await fetch(this.latestReleaseURL);
-        if (response.status !== 200) {
-            console.log(error);
+        try {
+            let response = await fetch(this.latestReleaseURL);
+            if (response.status !== 200) {
+                return;
+            }
+            this.release = await response.json();
+        } catch (error) {
+            this.release = undefined;
         }
-        this.release = await response.json();
     },
     methods: {
         releaseUrl: function(platform) {
