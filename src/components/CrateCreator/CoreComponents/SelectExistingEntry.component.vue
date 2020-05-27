3e69f948a500b7886841fbd5f18644f1d798c1a5
Marco La Rosa <m@lr.id.au>
2020-05-27 11:11:07 +1000

    styling

--- src/components/CrateCreator/CoreComponents/SelectExistingEntry.component.vue
+++ src/components/CrateCreator/CoreComponents/SelectExistingEntry.component.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col my-4">
-        <div class="text-sm text-gray-600">
+        <div class="text-sm text-center text-gray-600">
             Select a {{ type.toLowerCase() }} that is a part of this crate.
         </div>
         <el-select
@@ -17,15 +17,16 @@
                 :value="item.uuid"
             >
                 <div class="flex flex-row">
-                    <div class="text-lg gray-600">{{ item.name }}</div>
-                    <div class="flex-grow"></div>
-                    <div class="text-xs">id: {{ item.uuid }}</div>
+                    <div class="text-base text-gray-700 mr-4">
+                        {{ item.name }}
+                    </div>
+                    <div class="text-xs">{{ item.uuid }}</div>
                 </div>
             </el-option>
         </el-select>
         <div
             v-if="!entries || !entries.length"
-            class="text-gray-800 font-light"
+            class="text-gray-700 font-light"
         >
             No entries of type '{{ type }}' available yet.
         </div>
